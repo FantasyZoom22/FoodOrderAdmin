7ea0c0fcdc9ac48def6669c7296dbb03df3fb0dd
FantasyZoom22 <93161122+FantasyZoom22@users.noreply.github.com>
2024-03-17 23:27:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template
-from bs4 import BeautifulSoup
-import requests
 
 app = Flask(__name__)
 
@@ -12,21 +10,7 @@ def index():
 # Route for admin_panel.html
 @app.route('/admin-panel')
 def admin_panel():
-    # Fetch admin_panel.html content from URL
-    admin_panel_url = "https://foodorderadmin.onrender.com/admin-panel.html"  # Replace with actual URL
-    response = requests.get(admin_panel_url)
-
-    if response.status_code == 200:
-        admin_html = response.text
-
-        # Parse admin_panel.html and extract items list
-        admin_soup = BeautifulSoup(admin_html, 'html.parser')
-        items_list = admin_soup.find_all('div', class_='item')
-
-        # Render admin_panel.html with items list
-        return render_template('admin-panel.html', items=items_list)
-    else:
-        return "Failed to fetch admin_panel.html content. Status code: {}".format(response.status_code)
+    return render_template('admin-panel.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
