146dce497df12397b88ef2e969152858959d67a7
FantasyZoom22 <93161122+FantasyZoom22@users.noreply.github.com>
2024-03-19 20:23:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,39 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# Replace 'username', 'password', 'host', 'port', and 'database_name' with your PostgreSQL credentials
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://noureddine:yeZ1JYO0d9SqDdMyVL2BW14xSivEXm1G@dpg-cnsu8p5a73kc73b713u0-a
 
-# Sample item data
-items_data = {
-    '1': {'name': 'Tako', 'quantity': 150, 'image_url': 'https://res.cloudinary.com/dsjjtnudl/image/upload/v1692470283/TG-Foods/Tako_148_qb6ayw.png'},
-    '2': {'name': 'Hotdog', 'quantity': 120, 'image_url': 'https://res.cloudinary.com/dsjjtnudl/image/upload/v1692470281/TG-Foods/Hotdog_148_iddyls.png'},
-    '3': {'name': 'Burger', 'quantity': 200, 'image_url': 'https://res.cloudinary.com/dsjjtnudl/image/upload/v1692470282/TG-Foods/Burger_148_vm6adu.png'},
-    '4': {'name': 'Fries', 'quantity': 250, 'image_url': 'https://res.cloudinary.com/dsjjtnudl/image/upload/v1692470285/TG-Foods/Fries_148_jxcly5.png'},
-    '5': {'name': 'Pizza', 'quantity': 300, 'image_url': 'https://res.cloudinary.com/dsjjtnudl/image/upload/v1692470283/TG-Foods/Pizza_148_jej5pz.png'},
-    '6': {'name': 'Coke', 'quantity': 50, 'image_url': 'https://res.cloudinary.com/dsjjtnudl/image/upload/v1692470282/TG-Foods/Coke_148_ajethz.png'}
-}
+:5432/itemsdb'
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    image_url = db.Column(db.String(200), nullable=False)
+
+# Create the database tables (only need to do this once)
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def index():
-    return render_template('index.html', items=items_data)
-
-@app.route('/admin-panel')
-def admin_panel():
-    return render_template('admin-panel.html')
+    items = Item.query.all()
+    return render_template('index.html', items=items)
 
 @app.route('/update-items', methods=['POST'])
 def update_items():
-    global items_data
-    
-    # Get form data
     item_name = request.form['itemName']
     item_quantity = int(request.form['itemQuantity'])
     item_image_url = request.form['itemImage']
     
-    # Generate a new item ID
-    new_item_id = str(len(items_data) + 1)
+    new_item = Item(name=item_name, quantity=item_quantity, image_url=item_image_url)
+    db.session.add(new_item)
+    db.session.commit()
     
-    # Create a new item entry
-    items_data[new_item_id] = {'name': item_name, 'quantity': item_quantity, 'image_url': item_image_url}
-    
-    # Redirect back to the admin panel
-    return redirect(url_for('admin-panel'))
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
