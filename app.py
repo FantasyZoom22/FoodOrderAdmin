256ebc2b0e2d4fc08315f57bd90d37512dc75616
FantasyZoom22 <93161122+FantasyZoom22@users.noreply.github.com>
2024-03-20 00:31:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 # Configure the database connection string
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://noureddine:yeZ1JYO0d9SqDdMyVL2BW14xSivEXm1G@dpg-cnsu8p5a73kc73b713u0-a:5432/itemsdb'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Suppress SQLAlchemy modification tracking
 
 # Initialize SQLAlchemy
 db = SQLAlchemy(app)
@@ -14,10 +13,11 @@ db = SQLAlchemy(app)
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
     image_url = db.Column(db.String(200), nullable=False)
 
-# Don't call db.create_all() here. Instead, manage migrations separately.
+# Create tables based on the defined models
+db.create_all()
 
 @app.route('/')
 def index():
@@ -31,18 +31,29 @@ def admin_panel():
 
 @app.route('/update-items', methods=['POST'])
 def update_items():
-    # Get form data
-    item_name = request.form['itemName']
-    item_price = float(request.form['itemPrice'])  # Convert price to float
-    item_image_url = request.form['itemImage']
-    
-    # Create a new Item instance
-    new_item = Item(name=item_name, price=item_price, image_url=item_image_url)
-    
-    # Add the new item to the database session
-    db.session.add(new_item)
-    db.session.commit()
-    
-    # Redirect back to the admin panel
-    return redirect(url_for('admin-panel'))
+    try:
+        # Get the JSON data sent from the client-side
+        data = request.get_json()
+        
+        # Iterate over each item in the received data
+        for item_data in data:
+            # Check if the item with the same name already exists in the database
+            existing_item = Item.query.filter_by(name=item_data['name']).first()
+            if existing_item:
+                # If the item exists, update its price and image URL
+                existing_item.price = item_data['price']
+                existing_item.image_url = item_data['image']
+            else:
+                # If the item does not exist, create a new item entry
+                new_item = Item(name=item_data['name'], price=item_data['price'], image_url=item_data['image'])
+                db.session.add(new_item)
+        
+        # Commit changes to the database
+        db.session.commit()
+        
+        return jsonify({'success': True})
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)})
 
+if __name__ == '__main__':
+    app.run(debug=True)
