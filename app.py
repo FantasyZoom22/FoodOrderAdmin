399becb51464190f24f361d408ac3ed511cf7e09
FantasyZoom22 <93161122+FantasyZoom22@users.noreply.github.com>
2024-03-21 00:29:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ db = SQLAlchemy(app)
 # Define the Item model
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    price = db.Column(db.Float, nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
     image_url = db.Column(db.String(200), nullable=False)
 
 # Create tables based on the defined models
